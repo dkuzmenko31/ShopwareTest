877d1df75e463f395627ba9bc2965090d1f92a11
dkuzmenko_virtua <d.kuzmenko@wearevirtua.com>
2021-08-19 20:08:33 +0200

    DK-I-added-new-plugin

---
+++ custom/plugins/SwagDeltaStore/src/Resources/app/storefront/dist/storefront/js/swag-delta-store.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["swag-delta-store"],{
+
+/***/ "MR0X":
+/*!***********************************************************************************!*\
+  !*** /app/custom/plugins/SwagDeltaStore/src/Resources/app/storefront/src/main.js ***!
+  \***********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTVIwWC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///MR0X\n");
+
+/***/ })
+
+},[["MR0X","runtime"]]]);
